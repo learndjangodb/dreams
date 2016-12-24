8f99aa5bc757a72a1701479359e7f8683ed9868b
sreekanthreddyv <root@localhost.localdomain>
2016-12-24 17:48:28 +0530

    Contact Page

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,18 +1,36 @@
 from django import forms
 from .models import DreamReal
 
+
 class LoginForm(forms.Form):
-	user = forms.CharField(max_length=30)
-	password  = forms.CharField(widget=forms.PasswordInput())
+    user = forms.CharField(max_length=30)
+    password = forms.CharField(widget=forms.PasswordInput())
 
-	def clean_message(self):
-		username = self.cleaned_data.get("username")
-		dbuser = DreamReal.objects.filter(name = username)
+    def clean_message(self):
+        username = self.cleaned_data.get("username")
+        dbuser = DreamReal.objects.filter(name=username)
 
-		if not dbuser:
-			raise forms.ValidationError("User Does not Exist")
-		return username
+        if not dbuser:
+            raise forms.ValidationError("User Does not Exist")
+        return username
+
 
 class ProfileForm(forms.Form):
-	name = forms.CharField(max_length=100)
-	picture = forms.ImageField()
+    name = forms.CharField(max_length=100)
+    picture = forms.ImageField()
+
+
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    content = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
+
+    # some extra (optional)
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['contact_name'].label = "Your Name"
+        self.fields['contact_email'].label = "Your email:"
+        self.fields['content'].label = "What do you want to say?"
--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'tmp%17ouf#cd4gf_u@w54kcc^9*r0=7e^_57%7h%m-f%l46t^j'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
@@ -114,10 +109,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'targetibps42@gmail.com'
+EMAIL_HOST_PASSWORD = 'sarath123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
